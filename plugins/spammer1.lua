fd6739e1b1eacde5133c6cba65ed51bc11227481
hfjgjfg <mohammadhaselmehri20202020@gmail.com>
2016-03-17 14:33:51 +0330

    Update spammer1.lua

--- plugins/spammer1.lua
+++ plugins/spammer1.lua
@@ -5,7 +5,7 @@ function run(msg, matches)
               return "sik baw Only Sudo Can Use it"       end
   local tex = matches[1]
   local sps = matches[2]
-  local sp = 'done by BlackLifeTM'
+  local sp = 'done by @mohammad20162015'
 
 for i=1, tex, 1 do
 
